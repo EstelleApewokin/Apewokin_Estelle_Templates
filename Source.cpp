15499b117ea857ee32629f3827fa3d7de23c5785
Estelle Apewokin <estelliosis@icloud.com>
2022-07-31 23:52:20 -0400

    Added comments, got rid of extra code in the halfInt function

--- Source.cpp
+++ Source.cpp
@@ -8,20 +8,22 @@
 
 using namespace std;
 
-template <typename T>
+template <typename T>   //defines template half function
 T half(T number) {
 	T result;
 	result = number / 2;
 	return result;
 }
 
-int halfInt(int);
+int halfInt(int); //prototype for int half function
 
 int main() {
+	//test values
 	double a = 7.0;
 	float b = 5.0f;
 	int c = 3;
 
+	//calls function with each value
 	cout << half(a) << endl;
 	cout << half(b) << endl;
 	cout << halfInt(c) << endl;
@@ -30,11 +32,11 @@ int main() {
 }
 
 int halfInt(int num) {
-	double temp = static_cast<double>(num)/2;
-	double halved = round(temp);
-	int fin;
+	double temp = static_cast<double>(num)/2; //converts to double to prevent truncation during integer devision
+	temp = round(temp); //rounds value
+	int fin; //integer variable to hold final value
 
-	fin = static_cast<int>(halved);
+	fin = static_cast<int>(temp); //converts halved number from a double to an integer
 
 	return fin;
 }
